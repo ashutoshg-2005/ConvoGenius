b19dd2106da4168d2c2ccc521e5a86d873ec4565
Ashutosh Gaharwar <ashutoshgaharwar05@gmail.com>
2025-07-09 16:23:13 +0530

    10 - Enhance responsive dialog functionality

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -4,6 +4,7 @@ import {  useSuspenseQuery } from "@tanstack/react-query";
 import { useTRPC } from "@/trpc/client"; 
 
 
+
 export const AgentView = () => {
     const trpc = useTRPC();
     const { data} = useSuspenseQuery(trpc.agents.getMany.queryOptions());
